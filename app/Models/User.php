2fa8a406eff16e9e672fbb7b4bd013f0817ae9ec
ahmed7873 <ahmedmohammedalaidroos@gmail.com>
2026-08-12 13:47:15 +0300

    phone in model

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-#[Fillable(['name', 'email', 'password', 'role', 'is_active', 'email_verified_at'])]
+#[Fillable(['name', 'email', 'password', 'role', 'is_active', 'email_verified_at', 'phone'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable implements JWTSubject, MustVerifyEmail
 {
